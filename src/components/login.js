f7c670cc0f8a67ed927dbe8143cdf0615de1de6c
elbelel <umarelbelel@gmail.com>
2020-08-17 01:18:06 +0100

    UI fixes

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,5 @@
 
 import React  from 'react';
-import { Form, Col, Row, Container, Button} from 'react-bootstrap'
 import { AuthContext } from '../context/authcontext';
 
 
@@ -24,13 +23,13 @@ export const Login = () => {
 	return(
         <div className="authContainer">
            
-            <Container>
-            <Row>
-                <Col xs={12} md={4}></Col>
-                <Col xs={6} md={4}>
+            <div className='container'>
+            <div className='row'>
+                <div className='col-md-4'></div>
+                <div className='col-md-4'>
                     <div className="formContainer-1">
                         <h3 className="mb-5">Welcome to CometChatApp</h3>
-                        <Row>
+                        <div className='row'>
                             <div className='ml-5' onClick={()=>{login('superhero1')}}>
                         <img src='https://data-us.cometchat.io/assets/images/avatars/ironman.png' height='50px' width='50px' alt='superher1'/>
                             <p>superhero1</p>
@@ -43,28 +42,25 @@ export const Login = () => {
                         <img src='https://data-us.cometchat.io/assets/images/avatars/spiderman.png' height='50px' width='50px' alt='superher3' />
                             <p>superhero3</p>
                             </div>
-                     </Row>
+                     </div>
                      <br></br>
-                        <Form onSubmit={formsubmit}>
-                            <Form.Group>
-                                <Form.Control value={username} type="text" placeholder="Username"  onChange={(e)=> setUsername(e.target.value)}/>
-                            </Form.Group>
-                            <Form.Group>
-                            </Form.Group>
-
-                            <Form.Group>
+                        <form onSubmit={formsubmit}>
+                            <div className='form-group'>
+                                <input className='form-control' value={username} type="text" placeholder="Username"  onChange={(e)=> setUsername(e.target.value)}/>
+                            </div>
+                            {/* <div className='form-group'> */}
                                 <div className="mt-5">
-                                    <Button type='submit' variant="secondary btn-action" size="lg" disabled={isSubmitting} block>
+                                    <button type='submit' className='button' variant="secondary btn-action" size="lg" disabled={isSubmitting}>
                                         LOGIN {isSubmitting ? (<i className="fa fa-spinner fa-spin"></i>) : null}
-                                    </Button>
+                                    </button>
                                 </div>
-                            </Form.Group>
-                        </Form>
+                            {/* </div> */}
+                        </form>
                     </div>
-                </Col>
-            </Row>
-            <Col xs={12} md={4}></Col>
-            </Container>
+                </div>
+            </div>
+            <div className='col-md-4'></div>
+            </div>
 
 
         </div>
